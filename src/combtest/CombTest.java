027567239852a96575447285fc6d627bb401a56c
chhit5249 <chhit5249@7JGN6Y1.Student.UGDSB.ED>
2016-12-09 13:07:03 -0500

    V1.0 - Recursive version of comb sort works, but doesn't show comparisons or loops.

--- src/combtest/CombTest.java
+++ src/combtest/CombTest.java
@@ -5,50 +5,74 @@
  */
 package combtest;
 
+import java.util.*;
+
 /**
- *
+ * This program tests the comb sort.
  * @author chhit5249
  */
 public class CombTest {
 
+    static int loops = 0;
+
     /**
+     * Everything happens here.
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Array Declaration
-        int[] nums = new int[]{151, 54, 89, 42, 12, 8, 7, 6, 5, 4, 2, 2, 1};
+        //Array/Variable Declaration
+        Random r = new Random();
+        int rand, total;
+        total = 12;
+        int[] nums = new int[total];
+
+        //Generate random numbers for the array
+        for (int x = 0; x < total; x++) {
+            rand = r.nextInt(512);
+            nums[x] = rand;
+        }
         
+
         //Output
         System.out.println("Original Array:");
         for (int a = 0; a < nums.length; a++) {
             System.out.print(nums[a] + " ");
         }
-        System.out.println("\n--------------------------------\nSorted Array:");
+        long t1 = System.currentTimeMillis();
         sort(nums, nums.length);
+        long time = System.currentTimeMillis() - t1;
+        System.out.println("\n---------------------------------------------------------------------"
+                + "\nSorted Array:");
         for (int a = 0; a < nums.length; a++) {
             System.out.print(nums[a] + " ");
         }
+        System.out.println("\n\nSorting took: " + time + "ms");
+
     }
 
-    public static void sort(int x[], int gap) {
-        //Setting variables; n is the length of the array and gap 
-        int n = x.length;
-        gap = (gap * 10) / 13;
+    /**
+     * This method is how the sorting happens, but recursively!<p>
+     * pre: called 
+     * post: array is sorted
+     * @param input Array
+     */
+    public static void sort(int[] input, int gap) {  
+        //Reduce gap if it is already > 1
+        if (gap > 1) {
+            gap = (int) (gap / 1.3);
+        }
 
-        //Loop
-        for (int i = 0; i < n - gap; i++) {
-            //Sorting - shows each step 
-            if (x[i] > x[i + gap]) {
-                int ph = x[i];
-                x[i] = x[i + gap];
-                x[i + gap] = ph;
-                for (int a = 0;a<n;a++)
-                {
-                    System.out.print(x[a]+", ");
-                }
-                System.out.println("");
-                sort(x, gap);
+        //For loop to go through array and swap via gap.
+        for (int i = 0; i + gap < input.length; i++) {
+            
+            //If swap is necessary, swap
+            if (input[i] > input[i + gap]) {
+                int t = input[i];
+                input[i] = input[i + gap];
+                input[i + gap] = t;
+                sort(input, gap);
             }
         }
+
     }
 }
